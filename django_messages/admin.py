2a2a36e32d07f8aab3c619e3d6037836795191ed
gjergjk71 <gjergjk71@gmail.com>
2018-07-18 17:37:36 +0200

    Registered Message to admin

--- django_messages/admin.py
+++ django_messages/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Conversation
+from .models import *
 
 # Register your models here.
 
 admin.site.register(Conversation)
+admin.site.register(Message)
