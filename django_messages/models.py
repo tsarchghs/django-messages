f623e6816e55a7a568e73b247a52f1e0e5976963
gjergjk71 <gjergjk71@gmail.com>
2018-07-18 17:44:42 +0200

    Added created,deleted fields to Message model

--- django_messages/models.py
+++ django_messages/models.py
@@ -17,7 +17,8 @@ class Message(models.Model):
 	conversation = models.ForeignKey(Conversation,on_delete=models.CASCADE)
 	from_user = models.ForeignKey(User,on_delete=models.CASCADE)
 	content = models.TextField()
-
+	created = models.DateTimeField(auto_now_add=True)
+	deleted = models.BooleanField(default=False)
 	def __str__(self):
 		return "{} - {}".format(self.conversation,self.from_user)
-		
+
