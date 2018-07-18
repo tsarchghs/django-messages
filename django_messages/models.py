bb18b1555e4f4b1ba881cff07458fcbbfc6b4873
gjergjk71 <gjergjk71@gmail.com>
2018-07-18 17:31:24 +0200

    Created Conversation model

--- django_messages/models.py
+++ django_messages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Conversation(models.Model):
+	name = models.CharField(max_length=150,blank=True)
+	participants = models.ManyToManyField(User)
+
+	def __str__(self):
+		if self.name == "":
+			return "".join([p.username + "," for p in self.participants.all()])
+		else:
+			return self.name
+
