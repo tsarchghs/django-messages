612781e184943f2e3e49c8e9dd54802fa380d3f0
gjergjk71 <gjergjk71@gmail.com>
2018-07-19 15:35:18 +0200

    Created DeleteMessage path

--- django_messages/urls.py
+++ django_messages/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	path("create_message/<conversation_pk>", views.CreateMessage.as_view(),name="CreateMessage"),
+	path("delete_message/<pk>",views.DeleteMessage.as_view(),name="DeleteMessage"),
 ]
